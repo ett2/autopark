d48a0c497396d534cad87b70409988a1a06dd75b
Demo User <debian@beaglebone.localdomain>
2022-12-05 03:31:03 +0000

    updated statechange.py

--- statechange/statechange.py
+++ statechange/statechange.py
@@ -17,25 +17,51 @@ GPIO.setup(state1, GPIO.IN)
 GPIO.setup(stateu, GPIO.IN)
 
 # output setup
-GPIO.setup('P8_11', GPIO.OUT) # final output to jetson (might want to encode somehow later)
+out0 = 'P8_13'
+out1 = 'P8_14'
+GPIO.setup(out0, GPIO.OUT) # final output to jetson (might want to encode somehow later)
+GPIO.setup(out1, GPIO.OUT) # final output to jetson (might want to encode somehow later)
+
+
+def statechange(rf_state, ultrasonic_state):
+    final_state = 0
+    if ultrasonic_state == 1:
+        final_state = 3
+    else:
+        final_state = rf_state
+    return final_state
+
 
 while True:
     time.sleep(1)
     # print("1: ", GPIO.input(state0))
     # print("0: ", GPIO.input(state1))
     rf_state = GPIO.input(state1) * 2 + GPIO.input(state0) 
-    print(rf_state)
+    #print(rf_state)
     ultrasonic_state = GPIO.input(stateu)
-    print(ultrasonic_state)
+    #print(ultrasonic_state)
     
     # final output stuff
     final_state = statechange(rf_state, ultrasonic_state)
+    print("Final state: ")
     print(final_state)
-
-def statechange(rf_state, ultrasonic_state):
-    final_state = 0
-    if ultrasonic_state == True:
-        final_state = 4
+    
+    if (final_state == 0):
+        GPIO.output(out1, GPIO.LOW)
+        GPIO.output(out0, GPIO.LOW)  
+    elif (final_state == 1):
+        GPIO.output(out1, GPIO.LOW)
+        GPIO.output(out0, GPIO.HIGH)  
+    elif (final_state == 2):
+        GPIO.output(out1, GPIO.HIGH)
+        GPIO.output(out0, GPIO.LOW)  
+    elif (final_state == 3):
+        GPIO.output(out1, GPIO.HIGH)
+        GPIO.output(out0, GPIO.HIGH)  
     else:
-        final_state = rf_state
-    return final_state
+        GPIO.output(out1, GPIO.HIGH)
+        GPIO.output(out0, GPIO.HIGH)
+        print("Error!")
+    print("Output LEDs: ")
+    print(GPIO.input(out1))
+    print(GPIO.input(out0))
